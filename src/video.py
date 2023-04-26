0c8c58157c4fe234f6fee1c246bb490f32071d45
AlisaV365 <127328313+AlisaV365@users.noreply.github.com>
2023-04-26 14:50:18 +0300

    homework 4. Создан файл src/video и в нем классы: Video и PLVideo

---
+++ src/video.py
@@ -0,0 +1,40 @@
+import os
+from googleapiclient.discovery import build
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+YT_API_KEY = os.getenv('YT_API_KEY')
+
+
+class Video:
+    api_key = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    def __init__(self, video_id: str):
+        self.video_id = video_id
+        self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                         id=self.video_id).execute()
+        self.video_title = self.video_response['items'][0]['snippet']['title']
+        self.video_url = f"https://www.youtube.com/watch?v={self.video_id}"
+        self.view_count = self.video_response['items'][0]['statistics']['viewCount']
+        self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+
+    # возваращает название видео
+    def __str__(self):
+        return self.video_title
+
+
+
+class PLVideo(Video):
+    # наследование с переопределением
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
+
+    # возваращает название видео
+    def __str__(self):
+        return self.video_title
+
+
