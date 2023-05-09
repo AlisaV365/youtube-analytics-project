9790af9a9ad98e16e705ee33968a5c2efacf5b87
AlisaV365 <127328313+AlisaV365@users.noreply.github.com>
2023-05-09 20:34:41 +0300

    homework 6. Внедрение в код блоков `try/except'

--- src/video.py
+++ src/video.py
@@ -1,5 +1,6 @@
 import os
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 from dotenv import load_dotenv
 
@@ -8,23 +9,35 @@ load_dotenv()
 YT_API_KEY = os.getenv('YT_API_KEY')
 
 
+
 class Video:
     api_key = os.getenv('YT_API_KEY')
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, video_id: str):
         self.video_id = video_id
-        self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                         id=self.video_id).execute()
-        self.video_title = self.video_response['items'][0]['snippet']['title']
-        self.video_url = f"https://www.youtube.com/watch?v={self.video_id}"
-        self.view_count = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+
+        try:
+            self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                             id=self.video_id).execute()
+            self.title = self.video_response['items'][0]['snippet']['title']
+            self.url = f"https://www.youtube.com/watch?v={self.video_id}"
+            self.view_count = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+        except:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
+
+        # except HttpError as error:
+        #     if error.video_response == 404:
+        #         print(f"Видео {self.video_id} не найдено")
+
 
     # возваращает название видео
     def __str__(self):
-        return self.video_title
-
+        return self.title
 
 
 class PLVideo(Video):
@@ -35,6 +48,4 @@ class PLVideo(Video):
 
     # возваращает название видео
     def __str__(self):
-        return self.video_title
-
-
+        return self.title
